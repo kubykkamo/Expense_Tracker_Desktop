15cd4a70d128d34c5c2e1136a4fe0f9364347ad3
kubykkamo <gamingstudio39@gmail.com>
2026-03-06 15:53:05 +0100

    Added a button for adding new transaction and getting back to the overview. New transactions is added into a transaction list and displayed in the table immediately, but missing its category in the table. TODO: Fix missing categories on new transactions

--- Expense_Tracker_Desktop/Form1.Designer.cs
+++ Expense_Tracker_Desktop/Form1.Designer.cs
@@ -29,32 +29,201 @@
         private void InitializeComponent()
         {
             dgvTransactions = new DataGridView();
+            lblBalance = new Label();
+            button1 = new Button();
+            panelOverview = new Panel();
+            panelAddTransaction = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            cmbCategory = new ComboBox();
+            chckIsIncome = new CheckBox();
+            txtAmount = new TextBox();
+            txtDescription = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvTransactions).BeginInit();
+            panelOverview.SuspendLayout();
+            panelAddTransaction.SuspendLayout();
             SuspendLayout();
             // 
             // dgvTransactions
             // 
             dgvTransactions.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvTransactions.Location = new Point(91, 49);
+            dgvTransactions.Location = new Point(165, 78);
             dgvTransactions.Name = "dgvTransactions";
-            dgvTransactions.Size = new Size(609, 390);
+            dgvTransactions.Size = new Size(600, 390);
             dgvTransactions.TabIndex = 0;
             dgvTransactions.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // lblBalance
+            // 
+            lblBalance.AutoSize = true;
+            lblBalance.Font = new Font("Sitka Display", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            lblBalance.Location = new Point(266, 23);
+            lblBalance.Name = "lblBalance";
+            lblBalance.Size = new Size(74, 28);
+            lblBalance.TabIndex = 1;
+            lblBalance.Text = "Balance";
+            lblBalance.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(519, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 46);
+            button1.TabIndex = 2;
+            button1.Text = "Nová platba";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += NewTransaction_Click;
+            // 
+            // panelOverview
+            // 
+            panelOverview.Controls.Add(dgvTransactions);
+            panelOverview.Controls.Add(button1);
+            panelOverview.Controls.Add(lblBalance);
+            panelOverview.Location = new Point(72, 43);
+            panelOverview.Name = "panelOverview";
+            panelOverview.Size = new Size(954, 505);
+            panelOverview.TabIndex = 3;
+            // 
+            // panelAddTransaction
+            // 
+            panelAddTransaction.Controls.Add(label3);
+            panelAddTransaction.Controls.Add(label2);
+            panelAddTransaction.Controls.Add(label1);
+            panelAddTransaction.Controls.Add(button3);
+            panelAddTransaction.Controls.Add(button2);
+            panelAddTransaction.Controls.Add(cmbCategory);
+            panelAddTransaction.Controls.Add(chckIsIncome);
+            panelAddTransaction.Controls.Add(txtAmount);
+            panelAddTransaction.Controls.Add(txtDescription);
+            panelAddTransaction.Location = new Point(72, 43);
+            panelAddTransaction.Name = "panelAddTransaction";
+            panelAddTransaction.Size = new Size(954, 505);
+            panelAddTransaction.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Location = new Point(165, 324);
+            button3.Name = "button3";
+            button3.Size = new Size(138, 33);
+            button3.TabIndex = 8;
+            button3.Text = "Uložit platbu";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Location = new Point(550, 333);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 52);
+            button2.TabIndex = 7;
+            button2.Text = "Zpět";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += Overview_Click;
+            // 
+            // cmbCategory
+            // 
+            cmbCategory.FormattingEnabled = true;
+            cmbCategory.Location = new Point(165, 263);
+            cmbCategory.Name = "cmbCategory";
+            cmbCategory.Size = new Size(121, 23);
+            cmbCategory.TabIndex = 6;
+            cmbCategory.SelectedIndexChanged += txtCategory_SelectedIndexChanged;
+            // 
+            // chckIsIncome
+            // 
+            chckIsIncome.AutoSize = true;
+            chckIsIncome.Location = new Point(302, 194);
+            chckIsIncome.Name = "chckIsIncome";
+            chckIsIncome.Size = new Size(65, 19);
+            chckIsIncome.TabIndex = 5;
+            chckIsIncome.Text = "Příjem?";
+            chckIsIncome.UseVisualStyleBackColor = true;
+            chckIsIncome.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // txtAmount
+            // 
+            txtAmount.Location = new Point(165, 190);
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(100, 23);
+            txtAmount.TabIndex = 4;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(165, 108);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(334, 23);
+            txtDescription.TabIndex = 1;
+            txtDescription.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.Location = new Point(165, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 23);
+            label1.TabIndex = 9;
+            label1.Text = "Popis";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.Location = new Point(165, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 23);
+            label2.TabIndex = 10;
+            label2.Text = "Částka";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label3.Location = new Point(165, 237);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 23);
+            label3.TabIndex = 11;
+            label3.Text = "Kategorie";
+            label3.Click += label3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 503);
-            Controls.Add(dgvTransactions);
+            ClientSize = new Size(1055, 602);
+            Controls.Add(panelOverview);
+            Controls.Add(panelAddTransaction);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dgvTransactions).EndInit();
+            panelOverview.ResumeLayout(false);
+            panelOverview.PerformLayout();
+            panelAddTransaction.ResumeLayout(false);
+            panelAddTransaction.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private DataGridView dgvTransactions;
+        private Label lblBalance;
+        private Button button1;
+        private Panel panelOverview;
+        private Panel panelAddTransaction;
+        private TextBox txtDescription;
+        private ComboBox cmbCategory;
+        private CheckBox chckIsIncome;
+        private TextBox txtAmount;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label3;
+        private Label label2;
     }
 }
--- Expense_Tracker_Desktop/Form1.cs
+++ Expense_Tracker_Desktop/Form1.cs
@@ -10,6 +10,42 @@ namespace Expense_Tracker_Desktop
             var loadedTransactions = _storage.LoadTransactions();
             var loadedCategories = _storage.LoadCategories();
             _account = new Account(loadedTransactions, loadedCategories);
+            
+
+            cmbCategory.DataSource = _account.Categories;
+            cmbCategory.DisplayMember = "Name";
+            UpdateBalance();
+            FormatTable();
+
+
+
+
+
+            dgvTransactions.DataBindingComplete += DgvTransactions_DataBindingComplete;
+        }
+
+        private void DgvTransactions_DataBindingComplete(object? sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            FormatTable();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateBalance()
+        {
+            lblBalance.Text = $"Celkový zůstatek: {_account.Balance} Kč.";
+        }
+
+        private void FormatTable()
+        {
             dgvTransactions.DataSource = _account.Transactions;
             dgvTransactions.Columns["Category"].Visible = false;
             dgvTransactions.Columns["Description"].HeaderText = "Popis";
@@ -17,9 +53,101 @@ namespace Expense_Tracker_Desktop
             dgvTransactions.Columns["Date"].HeaderText = "Datum";
             dgvTransactions.Columns["IsIncome"].HeaderText = "Příjem?";
             dgvTransactions.Columns["CategoryName"].HeaderText = "Kategorie";
+
+            if (dgvTransactions.Columns["Amount"] != null)
+            {
+
+                dgvTransactions.Columns["Amount"].DefaultCellStyle.Format = "C0";
+            }
+
+
+            foreach (DataGridViewRow radek in dgvTransactions.Rows)
+            {
+                if (radek.IsNewRow) continue;
+
+
+                bool jePrijem = (bool)radek.Cells["IsIncome"].Value;
+
+                if (jePrijem)
+                {
+
+                    radek.DefaultCellStyle.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    radek.DefaultCellStyle.BackColor = Color.LightSalmon;
+                }
+            }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void NewTransaction_Click(object sender, EventArgs e)
+        {
+
+            panelOverview.Visible = false;
+            panelAddTransaction.Visible = true;
+        }
+
+        private void Overview_Click(object sender, EventArgs e)
+        {
+            panelAddTransaction.Visible = false;
+            panelOverview.Visible = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string description = txtDescription.Text;
+            decimal amount = decimal.Parse(txtAmount.Text);
+            Category category = (Category)cmbCategory.SelectedItem;
+            bool isIncome = chckIsIncome.Checked;
+
+            var transaction = new Transaction
+            {
+                Description = description,
+                Amount = amount,
+                IsIncome = isIncome,
+                Category = category,
+                Date = DateTime.Now,
+
+            };
+
+            _account.Transactions.Add(transaction);
+
+            dgvTransactions.DataSource = null;
+            dgvTransactions.DataSource = _account.Transactions;
+
+            UpdateBalance();
+            FormatTable();
+
+            txtDescription.Clear();
+            txtAmount.Clear();
+            chckIsIncome.Checked = false;
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
